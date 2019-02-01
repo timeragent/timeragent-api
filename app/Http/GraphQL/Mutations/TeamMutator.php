7858535c7084043eeb0c77aaf6bb00f5e148d033
Andrew Vlasiuk <andrew@digitalidea.studio>
2019-02-01 15:47:28 +0200

    Closed #83 Unable to change rates of the team members in the project and rate of a new member of the team

--- app/Http/GraphQL/Mutations/TeamMutator.php
+++ app/Http/GraphQL/Mutations/TeamMutator.php
@@ -3,6 +3,7 @@
 namespace App\Http\GraphQL\Mutations;
 
 use App\Models\Team;
+use App\Models\User;
 
 class TeamMutator
 {
@@ -30,6 +31,8 @@ class TeamMutator
     {
         $team_data = $args['team'];
 
+        $team_users = collect($args['team']['users']);
+
         $team = Team::find($args['team']['uuid']);
 
         $team->name       = $team_data['name'];
@@ -37,9 +40,31 @@ class TeamMutator
         $team->owner_uuid = $team_data['ownerUuid'];
         $team->save();
 
-        $users_uuids = collect($args['team']['users'])
+        $users_uuids = $team_users
             ->pluck('uuid')
             ->toArray();
+
+        $diff_users = array_merge(
+            $team->users->pluck('uuid')->diff($users_uuids)->toArray(),
+            collect($users_uuids)->diff($team->users->pluck('uuid'))->toArray()
+        );
+
+        // Detach old users from team projects, attach new users to team projects
+        foreach ($team->projects as $project) {
+            foreach ($diff_users as $uuid) {
+                $user = User::find($uuid);
+
+                if ($user->projects()->where('uuid', $project->uuid)->first()) {
+                    $project->users()->detach($user->uuid);
+                } else {
+                    $project->users()->attach([
+                        $user->uuid => [
+                            'team_uuid' => $team->uuid,
+                        ]
+                    ]);
+                }
+            }
+        }
 
         $team->users()->sync($users_uuids);
 
